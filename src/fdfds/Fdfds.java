04b1a8303a67ecaebbcec0c0bcaf1de5558bc58a
dinuka00 <tdammage10@gmail.com>
2023-10-18 17:18:53 +0530

    1

---
+++ src/fdfds/Fdfds.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fdfds;
+
+/**
+ *
+ * @author tdamm
+ */
+public class Fdfds {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
